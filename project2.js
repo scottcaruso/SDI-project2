72738df92a9c5382e1febd8dbb2a4fb620cb2c2c
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-01 16:00:33 -0400

    Checking in .js file with comments around boolean operators for later use.

--- project2.js
+++ project2.js
@@ -1,3 +1,37 @@
 alert("JavaScript works!");
 
 // Adding a comment simply to modify the file for a test addition to the respository;
+
+/* Comments to remind myself about logical operation operators
+
+AND p && q
+OR p || q
+NOT !p
+XOR (p || q) && !(p && q)
+
+*/
+
+/* Practice problems
+Given p ∧ (q ∨ r)
+
+if (p && (q || r) ) {
+	console.log ("The expression is true");
+} else {
+	console.log ("The expression is false");
+}
+
+Given (p ∧¬ q) ∨ (q ∧ r)
+if ((p && !q) || (q && r) {
+	console.log ("The expression is true");
+} else {
+	console.log ("The expression is false");
+}
+
+Given ¬(p ∨ r)
+if !(p || r) {
+	console.log ("The expression is true");
+} else {
+	console.log ("The expression is false");
+}
+
+*/
